9bc29432e20ac8dca33d6e216ffc91ead2df97e8
郭之存 <guoplc@163.com>
2026-06-25 20:29:04 +0800

    clear

--- example/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
-<template>
-  <div class="min-h-screen bg-dark bg-gradient-to-br from-dark to-dark-lighter text-light">
-    <RouterView />
-  </div>
-</template>
